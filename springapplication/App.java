9d67a3eb7b9d0ccf7f49999f54e71b4d1b2df85f
ArChAnAs123 <62280590+ArChAnAs123@users.noreply.github.com>
2021-09-05 16:51:42 +0530

    Update App.java

--- springapplication/App.java
+++ springapplication/App.java
@@ -1,1 +1,10 @@
+package com.sprinapplication.springapp;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
